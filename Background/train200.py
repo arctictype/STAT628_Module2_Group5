7781e6c4756f0c2a7bae2e13f26c6926044b9b96
Crystallc <clu78@wisc.edu>
2018-02-21 22:40:14 -0600

    train200 modify
    add dictionary part

--- Background/train200.py
+++ Background/train200.py
@@ -8,6 +8,8 @@ import numpy as np
 import os
 import re
 import langid
+from sklearn.feature_extraction.text import CountVectorizer
+import operator
 # first 200 lines of the train dataset
 train_200 = pd.read_csv("first200.csv")
 stops = set(stopwords.words("english"))
@@ -19,4 +21,13 @@ for i in range(len(train_200)):
     t3 = [porter_stemmer.stem(w) for w in t2]
     train_200["text"][i] = " ".join(t3)
 
+# get a dictionary
+def get_dict(data): # e.g. data = train_200["text"]
+    wordsbox = []
+    for line in data:
+        wordsbox.extend(line.split())
+    return collections.Counter(wordsbox)
 
+vocabulary = get_dict(train_200["text"])
+# e.g. get the first ten mostly used words
+sorted(vocabulary.items(),key=lambda k:k[1], reverse=True)[:11] 
