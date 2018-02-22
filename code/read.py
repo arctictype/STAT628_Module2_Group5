5cf0191b9d158f483079ec920720095bfdda75b2
lwang535 <wangl1995@yahoo.com>
2018-02-22 13:35:50 -0600

    Update read.py

--- code/read.py
+++ code/read.py
@@ -7,3 +7,6 @@ filename = 'train_data.csv'
 t1 = time.time()
 time.time() - t1
 reviews = pd.read_csv('train_data.csv')
+
+rev_200 = reviews.iloc[0:200,]
+rev_200.to_csv("first200.csv", index = False)
